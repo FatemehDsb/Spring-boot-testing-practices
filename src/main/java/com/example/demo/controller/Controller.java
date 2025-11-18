91f451f6fd13671ceb6c4128df61aec3e9db7b04
nz-bill <bill.palmestedt@zocom.se>
2025-11-18 09:46:37 +0100

    added basic security

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -5,8 +5,10 @@ import com.example.demo.model.Greeting;
 import com.example.demo.service.Service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/public")
 @RestController
 public class Controller {
 
